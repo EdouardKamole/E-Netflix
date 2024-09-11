f89aa63980f95d97e78d28fdb720f6152762901f
EdouardKamole <eddy.progammer@gmail.com>
2024-09-11 21:33:00 +0300

    Add Firebase configuration and restructure screens; update various project files

--- src/Nav.js
+++ src/Nav.js
@@ -20,7 +20,11 @@ function Nav() {
   return (
     <div className={`nav ${show && "nav_black"}`}>
       <div className="nav_contents">
-        <img className="nav_logo" src="netflix_logo.png" alt="Netflix Logo" />
+        <img
+          className="nav_logo"
+          src="e-netstream.png"
+          alt="E-netstream Logo"
+        />
         <img
           className="nav_avatar"
           src="https://i.pinimg.com/564x/32/3e/cc/323ecca68b7105d23184e783b86b0c5a.jpg"
